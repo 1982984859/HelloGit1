c3070425de37a9541bcbaad8b47be00fc42793fc
1982984859 <1982984859@qq.com>
2019-04-14 10:46:04 +0800

    dom2upload

--- src/com/git/text/text.java
+++ src/com/git/text/text.java
@@ -4,6 +4,28 @@ package com.git.text;
  * Created by Administrator on 2019/4/14 0014.
  */
 public class text {
+    /*
+    0.idea中创建仓库
+       vcs_Iivc_SpoG
+       自动生成一个主干——master
+
+    0.1 idea中上传文件到分
+
+       Git__commit file--commit and pull
+       <input value="分支名称"/
+
+
+    * 1.git分支
+    *
+    *     ·主干创建分支，各分支共享主干资源
+    *
+    *     ·分支修改项目&&git仅该分支修改，与主干，其他分支无关
+    *
+    *     ·拉取请求，到主干，分支新内容添加到主干
+    *    ·（由于主干，资源分支共享，此时其他分支也会因为主干的改变而改变）
+    *
+    *
+    * */
     public static void main(String[] args) {
         System.out.println("我是text类将要上传到git1");
     }
