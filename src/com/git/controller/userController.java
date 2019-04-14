207df5db97055de70410d37f5ef486a921c69671
1982984859 <1982984859@qq.com>
2019-04-14 11:09:29 +0800

    controller

--- src/com/git/controller/userController.java
+++ src/com/git/controller/userController.java
@@ -6,5 +6,6 @@ package com.git.controller;
 public class userController {
     public static void main(String[] args) {
         System.out.println("userController");
+        System.out.println("1");
     }
 }
